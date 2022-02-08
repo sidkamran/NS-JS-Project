3341731b558f9c8d88c079962b6303a2b85f5438
sidkamran <96372102+sidkamran@users.noreply.github.com>
2022-02-08 23:14:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      <div id="root">
+        <p>I am learning React. My life is getting better.</p>
+      </div>
+      </>
+    )
+  }
+}
+export default App;
